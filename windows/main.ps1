9edda6ae9b11e7075204b3da225bbd676ffebced
nightblade9 <nightbladecodes@gmail.com>
2022-04-15 15:43:07 -0400

    Update main.ps1

--- windows/main.ps1
+++ windows/main.ps1
@@ -1,6 +1,7 @@
 ###
 # Entry-point for Powershell. Relies on Choco, obviously. Make sure it's idempotent.
-# Can't replace with Python, because it's not installed. Oof.
+# 1) Can't replace with Python, because it's not installed. Oof.
+# 2) Some of these (e.g. Steam) don't show up in "choco list" so we can't check for installation before (re)installing.
 ###
 
 #Requires -RunAsAdministrator
@@ -8,6 +9,7 @@
 # From Chocolatey.org/install
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-foreach ($package in @('notepadplusplus', 'git', 'vscode', 'python')) {
+foreach ($package in @('vivaldi', 'notepadplusplus', 'git', 'vscode', 'python', 'steam-client', 'epicgameslauncher')) {
     choco install $package -y
+    refreshenv
 }
