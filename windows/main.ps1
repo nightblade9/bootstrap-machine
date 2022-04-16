63d93bc4ca93b47ec78dbf711473019a8420084c
nightblade9 <nightbladecodes@gmail.com>
2022-04-16 17:21:20 -0400

    Update main.ps1

--- windows/main.ps1
+++ windows/main.ps1
@@ -8,11 +8,7 @@
 # From Chocolatey.org/install
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-foreach ($package in @('godot', 'notepadplusplus', 'git', 'vscode', 'python', 'steam-client', 'epicgameslauncher')) {
+foreach ($package in @('godot', 'notepadplusplus', 'git', 'vscode', 'python', 'steam-client', 'epicgameslauncher', 'everything')) {
     choco install $package -y
     refreshenv
 }
-
-# Configure.
-git config --global user.name nightblade9
-git config --global user.email nightbladecodes@gmail.com
