2ec6450fd406c8a36e539d9cc50054ece1f5dd51
nightblade9 <nightbladecodes@gmail.com>
2022-04-22 00:59:26 -0400

    Create configure.sh

---
+++ linux/configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Add a swap file for hibernation. Assumes 16GB RAM. See: https://wiki.manjaro.org/index.php/Swap/en
+sudo dd if=/dev/zero of=/swapfile bs=1M count=4096 status=progress
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo bash -c "echo /swapfile none swap defaults 0 0 >> /etc/fstab"
