27c6299143d77d84ff4239f6e724ce7e799f6f13
nightblade9 <nightbladecodes@gmail.com>
2022-04-25 13:32:59 -0400

    Update main.sh

--- linux/main.sh
+++ linux/main.sh
@@ -17,3 +17,7 @@ sudo bash -c "echo /swapfile none swap defaults 0 0 >> /etc/fstab"
 findmnt -no UUID -T /swapfile # gives the hibernation device UUID
 sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}' # gives the offset
 # Run sudo vi /etc/default/grub and append this (with the values above) to GRUB_CMDLINE_LINUX_DEFAULT: resume=UUID=<uuid> resume_offset=<offset>
+
+# Tell Linux that the system time is local, not UTC, so that rebooting and changing OSes doesn't butcher the time
+# See: https://itsfoss.com/wrong-time-dual-boot/
+sudo timedatectl set-local-rtc 1
