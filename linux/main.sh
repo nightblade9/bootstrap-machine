71b063f4596d1d8785bbd2f23fab08edd5fbb554
nightblade9 <nightbladecodes@gmail.com>
2022-04-25 09:12:31 -0400

    Fix wrong swap file size

--- linux/main.sh
+++ linux/main.sh
@@ -9,7 +9,7 @@ git lfs install
 
 # Add a swap file for hibernation. Assumes 16GB RAM. See: https://wiki.manjaro.org/index.php/Swap/en
 # This also enables hibernation. (Previous versions of Manjaro required additional steps.)
-sudo dd if=/dev/zero of=/swapfile bs=1M count=4096 status=progress
+sudo dd if=/dev/zero of=/swapfile bs=1M count=20480 status=progress
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
