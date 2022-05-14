4069a1b1e6f433cbf2626dc621105529987b3123
nightblade9 <nightbladecodes@gmail.com>
2022-05-14 15:06:16 -0400

    Update main.sh

--- linux/main.sh
+++ linux/main.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Tell Linux that the system time is local, not UTC, so that rebooting and changing OSes doesn't butcher the time
+# See: https://itsfoss.com/wrong-time-dual-boot/
+sudo timedatectl set-local-rtc 1
+
+# Get and build wifi driver
+cd /tmp
+git clone https://github.com/brektrou/rtl8821CU
+cd rtl8821CU
+sudo pacman -S dkms
+./dkms-install.sh
+
+echo plug in wifi now
+
+# Initialize pacman and upgrade everything
+sudo pacman -Sy
+
+# Remove cruft
+sudo pacman -R thunderbird
+
+# Upgrade everything
+pacman -Syu
+
 # Install *everything!* Assumes you're on Arch/Manjaro.
 sudo pacman -S code godot gimp audacity lmms git-lfs
 
@@ -17,7 +39,3 @@ sudo bash -c "echo /swapfile none swap defaults 0 0 >> /etc/fstab"
 findmnt -no UUID -T /swapfile # gives the hibernation device UUID
 sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}' # gives the offset
 # Run sudo vi /etc/default/grub and append this (with the values above) to GRUB_CMDLINE_LINUX_DEFAULT: resume=UUID=<uuid> resume_offset=<offset>
-
-# Tell Linux that the system time is local, not UTC, so that rebooting and changing OSes doesn't butcher the time
-# See: https://itsfoss.com/wrong-time-dual-boot/
-sudo timedatectl set-local-rtc 1
